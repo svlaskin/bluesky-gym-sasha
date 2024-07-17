2283ac4944df242f48a4f7dc9f134cfc538affd2
jangroter <47716241+jangroter@users.noreply.github.com>
2024-07-17 16:48:07 +0200

    fixed bug in functions

--- bluesky_gym/envs/common/functions.py
+++ bluesky_gym/envs/common/functions.py
@@ -58,7 +58,6 @@ def random_point_on_circle(radius: float) -> np.array:
     y = radius * np.sin(alpha)
     return np.array([x, y])
 
-
 def sort_points_clockwise(vertices: np.array) -> np.array:
     """ Sort the points in clockwise order
     Parameters
@@ -73,8 +72,7 @@ def sort_points_clockwise(vertices: np.array) -> np.array:
     """
     sorted_vertices = [vertices[i] for i in np.argsort([np.arctan2(v[1], v[0]) for v in vertices])]
 
-    return sorted_vertices
-    
+    return sorted_vertices   
 
 def polygon_area(vertices: np.array) -> float:
     """ Calculate the area of a polygon given the vertices
@@ -177,45 +175,4 @@ def get_hdg(point1: np.array, point2: np.array) -> float:
     hdg = (hdg + 360) % 360 # Convert back to [0, 360] interval
     
     return hdg
-    
-    import numpy as np
 
-def bound_angle_positive_negative_180(angle_deg: float) -> float:
-    """ maps any angle in degrees to the [-180,180] interval 
-    Parameters
-    __________
-    angle_deg: float
-        angle that needs to be mapped (in degrees)
-    
-    Returns
-    __________
-    angle_deg: float
-        input angle mapped to the interval [-180,180] (in degrees)
-    """
-
-    if angle_deg > 180:
-        return -(360 - angle_deg)
-    elif angle_deg < -180:
-        return (360 + angle_deg)
-    else:
-        return angle_deg
-
-def get_point_at_distance(lat1, lon1, d, bearing, R=6371):
-    """
-    lat: initial latitude, in degrees
-    lon: initial longitude, in degrees
-    d: target distance from initial
-    bearing: (true) heading in degrees
-    R: optional radius of sphere, defaults to mean radius of earth
-
-    Returns new lat/lon coordinate {d}km from initial, in degrees
-    """
-    lat1 = np.radians(lat1)
-    lon1 = np.radians(lon1)
-    a = np.radians(bearing)
-    lat2 = np.arcsin(np.sin(lat1) * np.cos(d/R) + np.cos(lat1) * np.sin(d/R) * np.cos(a))
-    lon2 = lon1 + np.arctan2(
-        np.sin(a) * np.sin(d/R) * np.cos(lat1),
-        np.cos(d/R) - np.sin(lat1) * np.sin(lat2)
-    )
-    return np.degr
