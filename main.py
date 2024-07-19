f27ef019c7281414349b5195cf4f316c253e8985
Giulia Leto <49953200+giulialeto@users.noreply.github.com>
2024-07-19 14:25:09 +0200

    fixed issue with checking inside obstacles

--- main.py
+++ main.py
@@ -26,12 +26,12 @@ log_dir = f'./logs/{env_name}/'
 file_name = f'{env_name}_{str(algorithm.__name__)}.csv'
 csv_logger_callback = logger.CSVLoggerCallback(log_dir, file_name)
 
-TRAIN = False
+TRAIN = True
 EVAL_EPISODES = 10
 EPOCHS = 200
 
 if __name__ == "__main__":
-    env = gym.make(env_name, render_mode=None)
+    env = gym.make(env_name, render_mode='human')
     obs, info = env.reset()
     model = algorithm("MultiInputPolicy", env, verbose=1,learning_rate=3e-4)
     if TRAIN:
@@ -49,8 +49,8 @@ if __name__ == "__main__":
         obs, info = env.reset()
         tot_rew = 0
         while not (done or truncated):
-            action = np.array(np.random.randint(-100,100,size=(2))/100)
-            # action, _states = model.predict(obs, deterministic=True)
+            # action = np.array(np.random.randint(-100,100,size=(2))/100)
+            action, _states = model.predict(obs, deterministic=True)
             obs, reward, done, truncated, info = env.step(action[()])
             tot_rew += reward
         print(tot_rew)
