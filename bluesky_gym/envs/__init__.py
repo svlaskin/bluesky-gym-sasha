a89788fb1bebd71817e1d374846b8c49a1860501
jangroter <47716241+jangroter@users.noreply.github.com>
2024-07-29 09:55:27 +0200

    bug in __init__.py

--- bluesky_gym/envs/__init__.py
+++ bluesky_gym/envs/__init__.py
@@ -1,9 +1,7 @@
 from bluesky_gym.envs.descent_env import *
 from bluesky_gym.envs.plan_waypoint_env import *
-
 from bluesky_gym.envs.static_obstacle_env import *
 from bluesky_gym.envs.vertical_cr_env import *
-
 from bluesky_gym.envs.horizontal_cr_env import *
-from bluesky_gym.envs.aman_env import *
+from bluesky_gym.envs.aman_env_single import *
 from bluesky_gym.envs.sector_cr_env import *
