8c37c78f23da927862a1cfc82043156201f686c4
Giulia Leto <49953200+giulialeto@users.noreply.github.com>
2024-07-29 11:55:13 +0200

    added models and logs with different episode length

--- plot_test.py
+++ plot_test.py
@@ -18,10 +18,26 @@ ave_window = 100
 # drift001int10 = pd.read_csv(f'logs/{env}/drift001int10.csv')
 # cont = pd.read_csv(f'logs/{env}/continue.csv')
 # test = pd.read_csv(f'logs/{env}/test.csv')
-sac = pd.read_csv(f'logs/{env}/{env}_SAC.csv')
-sac2 = pd.read_csv(f'logs/{env}/{env}_SAC_2.csv')
+sac = pd.read_csv(f'logs_backup/{env}/{env}_SAC.csv')
+ppo = pd.read_csv(f'logs_backup/{env}/{env}_PPO.csv')
+ddpg = pd.read_csv(f'logs_backup/{env}/{env}_DDPG.csv')
+td3 = pd.read_csv(f'logs_backup/{env}/{env}_TD3.csv')
+
+sac_300 = pd.read_csv(f'logs_backup/{env}/{env}_SAC-max-ep-300.csv')
+ppo_300 = pd.read_csv(f'logs_backup/{env}/{env}_PPO-max-ep-300.csv')
+ddpg_300 = pd.read_csv(f'logs_backup/{env}/{env}_DDPG-max-ep-300.csv')
+td3_300 = pd.read_csv(f'logs_backup/{env}/{env}_TD3-max-ep-300.csv')
+
+sac_500 = pd.read_csv(f'logs_backup/{env}/{env}_SAC-max-ep-500.csv')
+ppo_500 = pd.read_csv(f'logs_backup/{env}/{env}_PPO-max-ep-500.csv')
+ddpg_500 = pd.read_csv(f'logs_backup/{env}/{env}_DDPG-max-ep-500.csv')
+td3_500 = pd.read_csv(f'logs_backup/{env}/{env}_TD3-max-ep-500.csv')
 
 name = 'total_reward'
+#'total_reward'
+#'waypoint_reached'
+#'crashed'
+#'average_drift'
 
 # plt.plot(drift01['timesteps'][:-(ave_window-1)],moving_average(drift01[name],ave_window),label='drift01')
 # plt.plot(drift001['timesteps'][:-(ave_window-1)],moving_average(drift001[name],ave_window),label='drift001')
@@ -29,7 +45,20 @@ name = 'total_reward'
 # plt.plot(cont['timesteps'][:-(ave_window-1)],moving_average(cont[name],ave_window),label='cont')
 # plt.plot(test['timesteps'][:-(ave_window-1)],moving_average(test[name],ave_window),label='test')
 plt.plot(sac['timesteps'][:-(ave_window-1)],moving_average(sac[name],ave_window),label='sac')
-plt.plot(sac2['timesteps'][:-(ave_window-1)],moving_average(sac2[name],ave_window),label='sac2')
+plt.plot(ppo['timesteps'][:-(ave_window-1)],moving_average(ppo[name],ave_window),label='ppo')
+plt.plot(ddpg['timesteps'][:-(ave_window-1)],moving_average(ddpg[name],ave_window),label='ddpg')
+plt.plot(td3['timesteps'][:-(ave_window-1)],moving_average(td3[name],ave_window),label='td3')
+
+# plt.plot(sac_300['timesteps'][:-(ave_window-1)],moving_average(sac_300[name],ave_window),label='sac_300')
+# plt.plot(ppo_300['timesteps'][:-(ave_window-1)],moving_average(ppo_300[name],ave_window),label='ppo_300')
+# plt.plot(ddpg_300['timesteps'][:-(ave_window-1)],moving_average(ddpg_300[name],ave_window),label='ddpg_300')
+# plt.plot(td3_300['timesteps'][:-(ave_window-1)],moving_average(td3_300[name],ave_window),label='td3_300')
+
+# plt.plot(sac_500['timesteps'][:-(ave_window-1)],moving_average(sac_500[name],ave_window),label='sac_500')
+# plt.plot(ppo_500['timesteps'][:-(ave_window-1)],moving_average(ppo_500[name],ave_window),label='ppo_500')
+# plt.plot(ddpg_500['timesteps'][:-(ave_window-1)],moving_average(ddpg_500[name],ave_window),label='ddpg_500')
+# plt.plot(td3_500['timesteps'][:-(ave_window-1)],moving_average(td3_500[name],ave_window),label='td3_500')
+
 # ppo = pd.read_csv(f'logs/{env}/{env}_PPO.csv')
 # sac = pd.read_csv(f'logs/{env}/{env}_SAC.csv')
 # td3 = pd.read_csv(f'logs/{env}/{env}_TD3.csv')
