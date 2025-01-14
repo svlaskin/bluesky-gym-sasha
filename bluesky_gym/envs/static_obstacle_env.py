4ae2ff31d53fae798a8ba727345f3ac988ace16d
jangroter <47716241+jangroter@users.noreply.github.com>
2025-01-14 16:50:24 +0100

    Changed usage of TAS to CAS for airspeed related actions

--- bluesky_gym/envs/static_obstacle_env.py
+++ bluesky_gym/envs/static_obstacle_env.py
@@ -341,7 +341,7 @@ class StaticObstacleEnv(gym.Env):
         dh = action[0] * D_HEADING
         dv = action[1] * D_SPEED
         heading_new = fn.bound_angle_positive_negative_180(bs.traf.hdg[bs.traf.id2idx('KL001')] + dh)
-        speed_new = (bs.traf.tas[bs.traf.id2idx('KL001')] + dv) * MpS2Kt
+        speed_new = (bs.traf.cas[bs.traf.id2idx('KL001')] + dv) * MpS2Kt
 
         bs.stack.stack(f"HDG {'KL001'} {heading_new}")
         bs.stack.stack(f"SPD {'KL001'} {speed_new}")
